9317acae6d3f9055744dfc5898f2011bb7dd0565
Robert Dixon <hrd2@st-andrews.ac.uk>
2013-10-26 16:15:59 +0100

    Fixed something small

--- public/facedetection/script.js
+++ public/facedetection/script.js
@@ -1,4 +1,4 @@
 function biauth(token) {
 	biauthWindow = window.open(
-	'http://biauthorize.azurewebsites.net/facedetection/?token='+token,'biauthorise','height=700,width=800,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=no');
+	'http://biauthorize.azurewebsites.net/facedetection/dialog.html?token='+token,'biauthorise','height=700,width=800,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=no');
 }
