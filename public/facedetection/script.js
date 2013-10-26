4d938e2a308c62418de82147ce98c46df49f2de0
Robert Dixon <hrd2@st-andrews.ac.uk>
2013-10-26 16:26:59 +0100

    A bit of magic

--- public/facedetection/script.js
+++ public/facedetection/script.js
@@ -1,4 +1,9 @@
 function biauth(token) {
 	biauthWindow = window.open(
-	'http://biauthorize.azurewebsites.net/facedetection/dialog.html?token='+token,'biauthorise','height=700,width=800,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=no');
+	'http://biauthorize.azurewebsites.net/facedetection/dialog.html','biauthorize','height=700,width=800,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=no');
+}
+
+function biauthorize(text){
+	document.write("<span><a id=\"biauth_authlink\" href=\"JavaScript:biauth();\">"+Text+"</a></span><br/>");
+
 }
