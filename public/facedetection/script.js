0e463ea015ce407e1868834f5085ecd27d7d3b5f
Robert Dixon <hrd2@st-andrews.ac.uk>
2013-10-27 12:05:51 +0000

    Uses Wolf's dialog

--- public/facedetection/script.js
+++ public/facedetection/script.js
@@ -1,6 +1,6 @@
 function biauth(token) {
 	biauthWindow = window.open(
-	'http://biauthorize.azurewebsites.net/facedetection/dialog.html','biauthorize','height=700,width=800,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=no');
+	'http://biauthorize.com:3000/biauthorize','Biauthorize','height=700,width=800,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=no');
 }
 
 function biauthorize(linktext){
